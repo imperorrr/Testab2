c54edbd26c8ba1d79f62b67c35c56c448c57b47a
Pёtr <150296585+imperorrr@users.noreply.github.com>
2024-01-26 19:13:29 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService ();
+        int index = service.calculate(81,1.81);
+        System.out.println(index);
+
+    }
 }
